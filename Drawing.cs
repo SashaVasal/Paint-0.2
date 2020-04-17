3bacbb67dd3c2f3a928125b0d9f4f2f93497784b
SashaVasal <34645577+SashaVasal@users.noreply.github.com>
2020-04-17 10:39:29 +1000

    new

--- Drawing.cs
+++ Drawing.cs
@@ -12,12 +12,12 @@ namespace Graph
 {
 
    
-    public class Figure
+    public class Figure : Tool 
     {
         public Point newPoint = new Point(0,0);
         public bool ActiveBrake = false;
 
-        public int count = 1;
+       
         //Square
         public void DrawSquare(PaintEventArgs e, Pen pen, Point StartPoint, Point MovePoint)
         {
@@ -68,12 +68,12 @@ namespace Graph
             g.DrawLine(pen, StartPoint.X, StartPoint.Y, MovePoint.X, MovePoint.Y);
         }
         //Pen 
-        public void Pen(Graphics g, Pen pen, Point MovePoint)
+        public void Pen(PaintEventArgs e, Pen pen, Point MovePoint)
         {
-            g.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y);
-            g.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y + 1);
-            g.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X + 1, newPoint.Y);
-            g.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y);
+            e.Graphics.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y);
+            e.Graphics.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y + 1);
+            e.Graphics.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X + 1, newPoint.Y);
+            e.Graphics.DrawLine(pen, MovePoint.X, MovePoint.Y, newPoint.X, newPoint.Y);
         }
         //Ellipse
         public void DrawEllipse(PaintEventArgs e, Pen pen, Point StartPoint, Point MovePoint)
@@ -85,19 +85,60 @@ namespace Graph
             g.DrawEllipse(pen, StartPoint.X, StartPoint.Y, MovePoint.X - StartPoint.X, MovePoint.Y - StartPoint.Y);
         }
         //Polyline
-        public void DrawPolyline(PaintEventArgs e, Pen pen, Point StartPoint, Point MovePoint)
+        public void DrawPolyline(PaintEventArgs e, Pen pen, Point MovePoint)
         {
             e.Graphics.DrawLine(pen, newPoint.X, newPoint.Y, MovePoint.X, MovePoint.Y);
         }
-        public void DrawPolyline(Graphics g, Pen pen,  Point StartPoint, Point MovePoint)
+        public void DrawPolyline(Graphics g, Pen pen, Point MovePoint)
         {
             g.DrawLine(pen, newPoint.X, newPoint.Y, MovePoint.X, MovePoint.Y);
         }
+        //Drawing
+        public void Draw(PaintEventArgs e)
+        {
+            switch (count)
+            {
+                case 1:
+                    Pen(e, pen, MovePoint);
+                    break;
+                case 2:
+                    DrawSquare(e, pen, StartPoint, MovePoint);
+                    break;
+                case 3:
+                    DrawEllipse(e, pen, StartPoint, MovePoint);
+                    break;
+                case 4:
+                    DrawLine(e, pen, StartPoint, MovePoint);
+                    break;
+                case 5:
+                    DrawPolyline(e, pen, MovePoint);
+                    break;
+            }
+        }
+        public void Draw(Graphics g)
+        {
+            switch (count)
+            {
+                
+                case 2:
+                    DrawSquare(g, pen, StartPoint, MovePoint);
+                    break;
+                case 3:
+                    DrawEllipse(g, pen, StartPoint, MovePoint);
+                    break;
+                case 4:
+                    DrawLine(g, pen, StartPoint, MovePoint);
+                    break;
+                case 5:
+                    DrawPolyline(g, pen, MovePoint);
+                    break;
+            }
+        }
     }
-    public class Tool : Figure
+    public class Tool
     {
-        
-           
+
+        public int count = 1;
         public int width_pen = 5;    
         public Color _color = Color.Black;
         public bool IsMouseDown = false;
@@ -109,45 +150,6 @@ namespace Graph
         public Pen pen = new Pen(Color.Black, 5);
 
         public Bitmap bit;
-        public void Draw(PaintEventArgs e)
-        {
-            switch (count)
-            {
-                          
-                case 2:
-                    DrawSquare(e, pen, StartPoint, MovePoint);
-                    break;
-                case 3:
-                    DrawEllipse(e, pen, StartPoint, MovePoint);
-                    break;
-                case 4:
-                    DrawLine(e, pen, StartPoint, MovePoint);
-                    break;
-                case 5:
-                    DrawPolyline(e, pen, StartPoint, MovePoint);
-                    break;
-            }
-        }
-        public void Draw(Graphics g)
-        {
-            switch (count)
-            {
-                case 1:
-                    Pen(g, pen, MovePoint);
-                    break;
-                case 2:
-                    DrawSquare(g, pen, StartPoint, MovePoint);
-                    break;
-                case 3:
-                    DrawEllipse(g, pen, StartPoint, MovePoint);
-                    break;
-                case 4:
-                    DrawLine(g, pen, StartPoint, MovePoint);
-                    break;
-                case 5:
-                    DrawPolyline(g, pen, StartPoint, MovePoint);
-                    break;
-            }
-        }
+        
     }
 }
